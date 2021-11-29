9eb881ac1975a8c64bf0378c66140d1eb20cee30
Nick <nicolai.trandafil@ellation.com>
2021-11-29 11:43:15 +0200

    set def route

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Route, Switch, Link } from "react-router-dom";
 import CV from "./CV";
-import Error from "./Error";
 
 const Routes = (props) =>
   console.log({ props, l: window.location }) || (
     <Switch>
-      <Route exact path="/cv" component={CV} />
-      <Route exact path="/404" component={Error} />
-      <Route component={Error} status="404" />
+      <Route component={CV} status="CV" />
     </Switch>
   );
 
