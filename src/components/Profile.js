25637ba7ac8180fef1bda8e05955d208516c5eca
Nick <nicolai.trandafil@ellation.com>
2021-11-30 23:41:18 +0200

    Add contacts

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,33 @@
 import React from "react";
 import Styled from "./Profile.styled";
 import Heading from "./Heading.styled";
+import Text from "./Text.styled";
 
 const Profile = () => (
-  <div>
+  <Styled.Profile>
     <Styled.Image src="hime.png" />
-    <Styled.Position as={Heading} size="m">
-      Senior Software Engineer
-    </Styled.Position>
-    <Styled.Name as={Heading} size="l">
-      {" "}
-      Trandafil Nicolai{" "}
-    </Styled.Name>
-  </div>
+    <Styled.NameWraper>
+      <Styled.Name size="l" isFixedSize as={Heading}>
+        Trandafil Nicolai
+      </Styled.Name>
+      <Heading as={Styled.Position} size="m">
+        Sr. Software Engineer
+      </Heading>
+    </Styled.NameWraper>
+    <Styled.Contacts>
+      <adress as={Text}>📍 Chisinau Moldova </adress>
+      <Text>⏳ GMT+2 </Text>
+      <a href="tel:+373-69-459-379" as={Text}>
+        📞 69459379
+      </a>
+      <a href="mailto:nikolai.trandafil@gmail.com" as={Text}>
+        ✉️ nikolai.trandafil@gmail.com
+      </a>
+      <a href="https://github.com/TNikolai" as={Text}>
+        Github emoji is needed
+      </a>
+    </Styled.Contacts>
+  </Styled.Profile>
 );
 
 export default Profile;
--- src/components/Profile.styled.js
+++ src/components/Profile.styled.js
@@ -4,16 +4,31 @@ import { Link } from "react-router-dom";
 
 const Styled = {};
 
-Styled.Image = styled.img`
-  color: lightblue;
+Styled.Profile = styled.div`
+  background-color: green;
+  display: flex;
 `;
 
-Styled.Position = styled.h2`
-  color: lightblue;
+Styled.Image = styled.img``;
+
+Styled.NameWraper = styled.div`
+  background-color: silver;
 `;
+
+Styled.Position = styled.h2``;
 
 Styled.Name = styled.h1`
-  color: lightblue;
+  font-weight: 800;
+  font-size: 2.35rem;
+  text-decoration: underline;
 `;
 
+Styled.Contacts = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem 1rem;
+  flex-wrap: wrap;
+  margin: 2rem;
+  background-color: white;
+`;
 export default Styled;
